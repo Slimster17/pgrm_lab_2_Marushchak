98e97a1a506d380ce294f49c384dfa5306116478
Slimster17 <96837677+Slimster17@users.noreply.github.com>
2024-03-09 10:29:00 +0200

    Initial commit

---
+++ pgrm_lab_2_marushchak/pgrm_lab_2_marushchak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pgrm_lab_2_marushchak
+//
+//  Created by Serhii on 08.03.24.
+//  Copyright © 2024 Serhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
